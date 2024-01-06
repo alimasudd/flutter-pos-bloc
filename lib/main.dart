dd704c39d6737382b88993c477d6a432f0042573
alimasudd <alimasudd.dev@gmail.com>
2024-01-06 10:22:02 +0700

    add BlocProvider LoginBloc(AuthRemoteDatasource()

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
-import 'package:flutter_pos/core/constants/colors.dart';
-import 'package:flutter_pos/presentation/auth/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_pos/core/assets/assets.gen.dart';
+import 'package:flutter_pos/data/datasources/auth_local_datasource.dart';
+import 'package:flutter_pos/data/datasources/auth_remote_datasource.dart';
+import 'package:flutter_pos/data/datasources/product_remote_datasource.dart';
+import 'package:flutter_pos/presentation/auth/pages/login_page.dart';
+import 'package:flutter_pos/presentation/home/bloc/product/product_bloc.dart';
+import 'package:flutter_pos/presentation/home/pages/dashboard_page.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'core/constants/colors.dart';
+import 'presentation/auth/bloc/login/login_bloc.dart';
+import 'presentation/home/bloc/logout/logout_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,30 +20,38 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter FIC Batch 11',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
-        useMaterial3: true,
-        textTheme: GoogleFonts.quicksandTextTheme(
-          Theme.of(context).textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => LoginBloc(AuthRemoteDatasource()),
         ),
-        appBarTheme: AppBarTheme(
-          color: AppColors.white,
-          elevation: 0,
-          titleTextStyle: GoogleFonts.quicksand(
-            color: AppColors.primary,
-            fontSize: 16.0,
-            fontWeight: FontWeight.w500,
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+          useMaterial3: true,
+          textTheme: GoogleFonts.quicksandTextTheme(
+            Theme.of(context).textTheme,
           ),
-          iconTheme: const IconThemeData(
-            color: AppColors.primary,
+          appBarTheme: AppBarTheme(
+            color: AppColors.white,
+            elevation: 0,
+            titleTextStyle: GoogleFonts.quicksand(
+              color: AppColors.primary,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+            ),
+            iconTheme: const IconThemeData(
+              color: AppColors.primary,
+            ),
           ),
         ),
+        home: LoginPage()
       ),
-      home: const LoginPage(),
     );
   }
 }
