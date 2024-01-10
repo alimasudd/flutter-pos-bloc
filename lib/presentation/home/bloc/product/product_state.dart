6586425ed1cffefd9451667a094fb1efa601cd85
alimasudd-mac <alimasudd.dev@gmail.com>
2024-01-10 16:47:32 +0700

    modified product_state.dart

--- lib/presentation/home/bloc/product/product_state.dart
+++ lib/presentation/home/bloc/product/product_state.dart
@@ -6,4 +6,5 @@ class ProductState with _$ProductState {
   const factory ProductState.loading() = _Loading;
   const factory ProductState.success(List<Product> products) = _Success;
   const factory ProductState.error(String message) = _Error;
+  
 }
