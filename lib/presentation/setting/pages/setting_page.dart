36028ac6fbfd4b34446063b22b26ce6f0ca62fb8
alimasudd-mac <alimasudd.dev@gmail.com>
2024-01-25 11:08:08 +0700

    modified setting_page.dart

--- lib/presentation/setting/pages/setting_page.dart
+++ lib/presentation/setting/pages/setting_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_pos/core/constants/colors.dart';
 import 'package:flutter_pos/core/extensions/build_context_ext.dart';
 import 'package:flutter_pos/data/datasources/auth_local_datasource.dart';
-import 'package:flutter_pos/data/datasources/product_local_datasource.dart';
 import 'package:flutter_pos/presentation/auth/pages/login_page.dart';
-import 'package:flutter_pos/presentation/home/bloc/product/product_bloc.dart';
-import 'package:flutter_pos/presentation/order/models/order_model.dart';
+import 'package:flutter_pos/presentation/setting/pages/manage_printer_page.dart';
+import 'package:flutter_pos/presentation/setting/pages/save_server_key_page.dart';
+import 'package:flutter_pos/presentation/setting/pages/sync_data_page.dart';
 
 import '../../../core/assets/assets.gen.dart';
 import '../../../core/components/menu_button.dart';
@@ -30,60 +29,54 @@ class _SettingPageState extends State<SettingPage> {
         ),
         body: Column(
           children: [
-            Row(
-              children: [
-                MenuButton(
-                  iconPath: Assets.images.manageProduct.path,
-                  label: 'Kelola Produk',
-                  onPressed: () => context.push(const ManageProductPage()),
-                  isImage: true,
-                ),
-                const SpaceWidth(15.0),
-                MenuButton(
-                  iconPath: Assets.images.managePrinter.path,
-                  label: 'Kelola Printer',
-                  onPressed:
-                      () {}, //=> context.push(const ManagePrinterPage()),
-                  isImage: true,
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  MenuButton(
+                    iconPath: Assets.images.manageProduct.path,
+                    label: 'Kelola Produk',
+                    onPressed: () => context.push(const ManageProductPage()),
+                    isImage: true,
+                  ),
+                  const SpaceWidth(15.0),
+                  MenuButton(
+                    iconPath: Assets.images.managePrinter.path,
+                    label: 'Kelola Printer',
+                    onPressed: () {
+                      context.push(const ManagePrinterPage());
+                    }, //=> context.push(const ManagePrinterPage()),
+                    isImage: true,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  MenuButton(
+                    iconPath: Assets.images.manageProduct.path,
+                    label: 'QRIS Server Key',
+                    onPressed: () => context.push(const SaveServerKeyPage()),
+                    isImage: true,
+                  ),
+                  const SpaceWidth(15.0),
+                  MenuButton(
+                    iconPath: Assets.images.managePrinter.path,
+                    label: 'Sinkronisasi Data',
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SyncDataPage()));
+                    }, //=> context.push(const ManagePrinterPage()),
+                    isImage: true,
+                  ),
+                ],
+              ),
             ),
             const SpaceHeight(60),
-            BlocConsumer<ProductBloc, ProductState>(
-              listener: (context, state) {
-                state.maybeMap(
-                  orElse: () {},
-                  success: (_) async {
-                    await ProductLocalDatasource.instance.removeAllProduct();
-                    await ProductLocalDatasource.instance
-                        .insertAllProduct(_.products.toList());
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        backgroundColor: AppColors.primary,
-                        content: Text(
-                          'Sync data success',
-                        )));
-                  },
-                );
-              },
-              builder: (context, state) {
-                return state.maybeWhen(
-                  orElse: () {
-                    return ElevatedButton(
-                        onPressed: () {
-                          context
-                              .read<ProductBloc>()
-                              .add(const ProductEvent.fetch());
-                        },
-                        child: const Text('Sync Data'));
-                  },
-                  loading: () {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                );
-              },
-            ),
             const Divider(),
             BlocConsumer<LogoutBloc, LogoutState>(
               listener: (context, state) {
@@ -108,25 +101,6 @@ class _SettingPageState extends State<SettingPage> {
               },
             ),
             const Divider(),
-            FutureBuilder<List<OrderModel>>(
-                future: ProductLocalDatasource.instance.getOrderByIsSync(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Expanded(
-                      child: ListView.builder(
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            title: Text(
-                                snapshot.data![index].paymentMethod.toString()),
-                          );
-                        },
-                        itemCount: snapshot.data!.length,
-                      ),
-                    );
-                  } else {
-                    return const SizedBox();
-                  }
-                }),
           ],
         ));
   }
